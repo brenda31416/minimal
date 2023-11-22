b5099d30853b8e34e7d49d5bcfab4e5479c60ca7
brenda31416 <141837475+brenda31416@users.noreply.github.com>
2023-11-22 14:39:54 -0600

    Update __init__.py

--- minimal/__init__.py
+++ minimal/__init__.py
@@ -6,3 +6,11 @@ def my_sum(x, y):
 def my_mul(x, y):
     """A function that multiply. """
     return x*y
+    
+def subconjuntos(conjunto):
+    todos = []
+    for i in range(len(conjunto)+1):
+        for sub in combinations(conjunto, i):
+            todos.append(set(sub))
+
+    return todos
